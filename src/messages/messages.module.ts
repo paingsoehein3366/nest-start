568f09fed9dc0d2007367569b8313ad6fa77e9d4
PaingSoeHein <pc@pishinoMacBook-Pro.local>
2024-04-30 14:10:51 +0900

    injectable

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MessagesController } from './controllers/messages.controller';
+import { MessagesController } from './messages.controller';
+import { MessagesRespository } from './messages.repository';
+import { MessagesService } from './messages.service';
 
 @Module({
-      controllers: [MessagesController]
+      controllers: [MessagesController],
+      providers: [MessagesService, MessagesRespository]
 })
 export class MessagesModule { }
